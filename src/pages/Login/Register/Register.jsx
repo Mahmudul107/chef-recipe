76322ba696c4abe4e7aae61e30e87c46e1d05b4b
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-02 21:27:43 +0600

    Register page added

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import { Input, Button } from "react-daisyui";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <p>Register page</p>
-        </div>
-    );
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // handle registration logic here
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-fuchsia-300 p-6 rounded-lg shadow-lg w-1/2 h-4/5">
+        <h2 className="text-lg font-semibold mb-4">Register</h2>
+        <form
+          onSubmit={handleRegister}
+          className="flex flex-col h-full space-y-4"
+        >
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <Input
+              type="text"
+              placeholder="Enter your name"
+              className="input w-full"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              className="input w-full"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              className="input w-full"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <Input
+              type="text"
+              placeholder="Enter your photo URL"
+              className="input w-full"
+            />
+          </div>
+          <Button type="submit" className="btn btn-primary w-full">
+            Register
+          </Button>
+          <p className="mb-12">
+            <small>
+              Already have an account ? please{" "}
+              <Link className="text-primary-500 hover:underline font-bold" to="/login">
+                Login
+              </Link>
+            </small>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
