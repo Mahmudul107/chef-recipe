fe417347c014846521f55ba9960a614491e13804
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-04 13:00:23 +0600

    applied lazy loader to the homepage 6 cards

--- src/pages/Home/Chefs/Chefs.jsx
+++ src/pages/Home/Chefs/Chefs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import LazyLoad from "react-lazy-load";
 import { Link } from "react-router-dom";
 
 const Chefs = ({ chef }) => {
@@ -12,7 +13,9 @@ const Chefs = ({ chef }) => {
 
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg m-4 bg-gray-200 hover:bg-red-200 duration-1000">
-      <img className="w-full" src={chef.chef_picture} alt={chef.chef_name} />
+      <LazyLoad height={200} offset={100} threshold={0.95} placeholder={<div>Loading...</div>}>
+        <img className="w-full" src={chef.chef_picture} alt={chef.chef_name} onError={(e) => e.target.src = 'default-image.jpg'} />
+      </LazyLoad>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{chef.chef_name}</div>
         <p className="text-gray-700 text-base mb-2">
